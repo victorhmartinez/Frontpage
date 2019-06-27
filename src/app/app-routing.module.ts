9a035094526ad620c4c78c69e3ea43e778f3afa4
Jose <jepullaguari@utpl.edu.ec>
2019-06-26 20:42:31 -0500

    Subiendo cambios componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdministracionComponent } from './components/administracion/administracion.component'
+import { FrontpageComponent } from './components/frontpage/frontpage.component'
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: '', component: AppComponent },
   { path: 'administracion', component: AdministracionComponent },
+  { path: 'frontpage', component: FrontpageComponent },
 ];
 
 @NgModule({
---
+++ src/app/components/frontpage/frontpage.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-frontpage',
+  templateUrl: './frontpage.component.html',
+  styleUrls: ['./frontpage.component.css'],
+  providers: [NgbModalConfig, NgbModal],
+  
+  styles: [`
+  .dark-modal .modal-content {
+    background-color: #292b2c;
+    color: white;
+  }
+  .dark-modal .close {
+    color: white;
+  }
+  `]
+})
+export class FrontpageComponent implements OnInit {
+
+  constructor(config: NgbModalConfig, private modalService: NgbModal, private router: Router) { 
+    // customize default values of modals used by this component tree
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
+
+  ngOnInit() {
+  }
+  open(content) {
+    this.modalService.open(content, { windowClass: 'dark-modal', size: 'sm' });
+  }
+
+  login(){
+    this.router.navigate(['/administracion']);    
+  } 
+}
